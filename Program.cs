3df58fbc3a450cf3443a3d374a6ac0fa77319c90
Johanness15 <89245458+Johanness15@users.noreply.github.com>
2022-12-04 21:57:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TryParse {
+    internal class Program {
+        public static void Main(string[] args) {
+            int result;
+            if (!int.TryParse(s: Console.ReadLine(), result: out result))
+                Console.Write(value: "Input a number "); 
+        }
+
+        public static bool TryParse(string s, out int result) {
+            bool parsed;
+            int tempRes = 0;
+            result = tempRes;
+
+            try {
+
+                result = Convert.ToInt32(value: s);
+                parsed = true;
+
+                if (parsed == true) {
+                    return true;
+                }
+
+            } catch (System.Exception) {
+
+                parsed = false;
+
+                if (parsed == false) {
+                    return false;
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
